e0cb75321faffc42bfe2806ea16b3e588bd11112
hamoci <hamoci@naver.com>
2025-09-23 15:41:17 +0900

    add omitssion build config

---
+++ build_binary_configs_rev.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+cd /home/hamoci/Study/ChampSim/
+
+# ./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_2MBPage_DRAM_Error_1e_4.json
+# make -j
+# ./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_2MBPage_DRAM_Error_1e_5.json
+# make -j
+# ./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_2MBPage_DRAM_Error_1e_6.json
+# make -j
+# ./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_2MBPage_DRAM_Error_1e_7.json
+# make -j
+# ./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_2MBPage_DRAM_Error_1e_8.json
+# make -j
+# ./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_2MBPage_DRAM_Error_1e_9.json
+# make -j
+# ./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_2MBPage_DRAM_Error_1e_10.json
+# make -j
+# ./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_2MBPage_DRAM_Error_1e_11.json
+# make -j
+
+
+# ./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_2MBPage_DRAM_Error_1e_12.json
+# make -j
+# ./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_2MBPage_DRAM_Error_1e_13.json
+# make -j
+# ./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_2MBPage_DRAM_Error_1e_14.json
+# make -j
+# ./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_2MBPage_DRAM_Error_1e_15.json
+# make -j
+
+./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_4KBPage_DRAM_Error_1e_4.json
+make -j
+./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_4KBPage_DRAM_Error_1e_5.json
+make -j
+./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_4KBPage_DRAM_Error_1e_6.json
+make -j
+./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_4KBPage_DRAM_Error_1e_7.json
+make -j
+./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_4KBPage_DRAM_Error_1e_8.json
+make -j
+./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_4KBPage_DRAM_Error_1e_9.json
+make -j
+./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_4KBPage_DRAM_Error_1e_10.json
+make -j
+./config.sh ./sim_configs/_32GBDRAM/_2MBLLC_4KBPage_DRAM_Error_1e_11.json
+make -j
+
